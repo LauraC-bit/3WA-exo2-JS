6e47de75e5c07da21b11e8801912ae09337d6b9b
Laura Comte <comtelaurapro@gmail.com>
2023-06-06 11:01:43 +0200

    correction

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,46 +1,37 @@
-
 const version = document.getElementById("version");
 // const menu = document.getElementById("menu");
-// let versionIsOpen = false; 
+let versionIsOpen = false;
 
 const clickMenu = (event) => {
-    const target = event.target;
-    const targetSibling = event.target.nextElementSibling;
+  const target = event.target;
+  const targetSibling = event.target.nextElementSibling;
 
-    target.classList.add("header__ul__version__dropdown");
-    // target.firstChild.classList.add("header__ul__version__dropdown");
-    targetSibling.classList.add("header__ul__version--hover");
+  target.classList.add("header__ul__version__dropdown");
+  // target.firstChild.classList.add("header__ul__version__dropdown");
+  targetSibling.classList.add("header__ul__version--hover");
 
-    // versionIsOpen = true;
-}
+  versionIsOpen = true;
+};
 
-
-
-version.addEventListener('click', clickMenu);
+version.addEventListener("click", clickMenu);
 // menu.addEventListener('click', clickMenu);
-
 
 // CORRECTION ---------------------------
 
 window.document.addEventListener("click", (event) => {
-//     // console.dir(event.target)
-//     // if (versionIsOpen) {
-//     //     return;
-//     // }
-    // const version2 = document.getElementById("version2");
-    const target = event.target;
-    const au_revoir = document.getElementById("au_revoir");
+  //     // console.dir(event.target)
+  if (versionIsOpen) {
+    return;
+  }
+  // const version2 = document.getElementById("version2");
+  const target = event.target;
+  const au_revoir = document.getElementById("au_revoir");
 
-    if (target.classList === "version2") {
-        au_revoir.classList.remove("header__ul__version--hover");
-    } else {
-        au_revoir.classList.remove("header__ul__version--hover");
-    }
-})
+  if (target.classList === "version2") {
+    au_revoir.classList.remove("header__ul__version--hover");
+  } else {
+    au_revoir.classList.remove("header__ul__version--hover");
+  }
+});
 
 // ----------------------------------------
-
-
-
-
-
